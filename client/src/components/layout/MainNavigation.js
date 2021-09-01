3976b221bfdea604a4d96eca1b1a1f47d3d34883
rcraig <ryanpcraig1@gmail.com>
2021-09-01 17:14:47 -0500

    Finished nav bar

--- client/src/components/layout/MainNavigation.js
+++ client/src/components/layout/MainNavigation.js
@@ -6,18 +6,16 @@ import Container from 'react-bootstrap/Container'
 function MainNavigation() {
   return (
     <header>
-      <Navbar className="navbar navbar-expand-lg navbar-light bg-light">
+      <Navbar bg="light" expand="sm">
         <Navbar.Brand>Budget App</Navbar.Brand>
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/add-expense">Add Expense</Nav.Link>
-              <Nav.Link href="/add-payment">Add Payment</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav classNa="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/add-expense">Add Expense</Nav.Link>
+            <Nav.Link href="/add-payment">Add Payment</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </header>
   )
