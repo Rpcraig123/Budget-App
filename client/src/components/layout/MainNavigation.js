d515314658ea0bdd2b80edff7120a8c47e840e16
rcraig <ryanpcraig1@gmail.com>
2021-09-01 15:49:22 -0500

    Started setting up nav bar

--- client/src/components/layout/MainNavigation.js
+++ client/src/components/layout/MainNavigation.js
@@ -1,41 +1,24 @@
-// import { Link } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 
-
 function MainNavigation() {
   return (
     <header>
-      {/* <Navbar className="navbar navbar-expand-sm navbar-light bg-light">
-        <Link to='/' className="navbar-brand">Home</Link>
-        <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarMenu">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to='/add-expense' className="nav-link">Add Expense</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/add-payment' className="nav-link">Add Payment</Link>
-            </li>
-          </ul> 
-        </div>
-      </Navbar> */}
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar className="navbar navbar-expand-lg navbar-light bg-light">
+        <Navbar.Brand>Budget App</Navbar.Brand>
         <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/add-expense">Add Expense</Nav.Link>
+              <Nav.Link href="/add-payment">Add Payment</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* <div className="Title">Budget App</div> */}
     </header>
   )
 }
