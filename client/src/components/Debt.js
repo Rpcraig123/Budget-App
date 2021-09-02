831d4591e718a7776d8e26ff91a69a5c6cb4d0a2
rcraig <ryanpcraig1@gmail.com>
2021-09-02 10:46:09 -0500

    removed comments

--- client/src/components/Debt.js
+++ client/src/components/Debt.js
@@ -9,7 +9,6 @@ function Debt(props) {
       <Card>
         <Card.Body>Total Debt = ${props.totalDebt}</Card.Body>
       </Card>
-      {/* <h2>Total Debt = ${props.totalDebt}</h2> */}
     </div>
   )
 }
