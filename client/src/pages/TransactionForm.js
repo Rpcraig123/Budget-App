bd0c4788daf67c13dee07405c63c0a689bc12a7f
rcraig <ryanpcraig1@gmail.com>
2021-09-02 12:09:08 -0500

    fixed soem styling

--- client/src/pages/TransactionForm.js
+++ client/src/pages/TransactionForm.js
@@ -1,5 +1,6 @@
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 function PaymentForm(props) {
 
@@ -18,26 +19,28 @@ function PaymentForm(props) {
 
   return (
     <div className='add-pay'>
-      <h3>{props.formType} Form</h3>
-      <Form onSubmit={(e) => props.createNewPost(e, props.transType)}>
-        <Form.Group className="mb-3">
-          <Form.Label>Year</Form.Label>
-          <Form.Control type="text" placeholder="Enter Year" onChange={(e) => yearOnChange(e)}/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Month</Form.Label>
-          <Form.Control type="text" placeholder="Enter Month" onChange={(e) => monthOnChange(e)}/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Description</Form.Label>
-          <Form.Control type="text" placeholder="Enter Description" onChange={(e) => descOnChange(e)}/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Amount</Form.Label>
-          <Form.Control type="text" placeholder="Enter Amount in USD" onChange={(e) => amountOnChange(e)}/>
-        </Form.Group>
-        <Button type="submit" variant="outline-primary">Submit</Button>
-      </Form>
+      <Container>
+        <h3>{props.formType} Form</h3>
+        <Form onSubmit={(e) => props.createNewPost(e, props.transType)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Year</Form.Label>
+            <Form.Control type="text" placeholder="Enter Year" onChange={(e) => yearOnChange(e)}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Month</Form.Label>
+            <Form.Control type="text" placeholder="Enter Month" onChange={(e) => monthOnChange(e)}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" placeholder="Enter Description" onChange={(e) => descOnChange(e)}/>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Amount</Form.Label>
+            <Form.Control type="text" placeholder="Enter Amount in USD" onChange={(e) => amountOnChange(e)}/>
+          </Form.Group>
+          <Button type="submit" variant="outline-primary">Submit</Button>
+        </Form>
+      </Container>
     </div>
   )
 }
